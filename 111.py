d181f005967b426a2ebad67fbfca55271ddb07e5
zOOm2705 <57778413+zOOm2705@users.noreply.github.com>
2024-05-24 20:57:50 +0600

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,19 @@
+class Human:
+    def __init__(self,FIO='Unknown',data='Unknown',phone="Unknown",city="Unknown",country="Unknown",adress="Unknown"):
+        self.FIO = FIO
+        self.data = data
+        self.phone = phone
+        self.city = city
+        self.country = country
+        self.adress = adress
+    def print_data(self):
+        return (f'Фамилия имя отчество: {self.FIO},\nДата рождения: {self.data},\nКонтактный телефон: {self.phone},\nГород: {self.city},\nСтрана: {self.country},\nДомашний адрес: {self.adress}')
+    def input_data(self):
+        self.FIO = input()
+    def print_FIO(self):
+        return (self.FIO)
+object1 = Human('Ivanov Inav Ivanovich','25.03.1992',"88005553535","Омск","Россия","Колотушкина 02-05")
+object2 = Human()
+object1.input_data()
+print(object1.print_FIO())
+print(object1.FIO)
